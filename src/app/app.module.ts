e2b106b23d3e308d374dbbdf43b6e188a5c48f8f
unknown <naveednasib@hotmail.com>
2019-03-15 16:48:35 +0500

    adding interpolation and test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    InterpolationComponent
   ],
   imports: [
     BrowserModule
